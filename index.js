5d97d01a4af7bc668b3dbcd5a84489d621f0c4d6
kotesh-arya <koteswarraomudila@gmail.com>
2021-09-21 14:54:04 +0530

    base logic updated

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-const paisa = document.querySelector("#bill-amount");
-console.log(paisa.value);
+const billAmount = document.querySelector("#bill-amount");
+var changeAmount = document.querySelector("#check-amount");
+
+changeAmount.addEventListener("click",()=>{
+    console.log(billAmount.value);
+});
