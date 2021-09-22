9919e0a1c4cf5ee68dedc5f74b769004171c5fce
kotesh-arya <koteswarraomudila@gmail.com>
2021-09-22 17:00:09 +0530

    updated input and output part in java script

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 const billAmount = document.querySelector("#bill-amount");
-var changeAmount = document.querySelector("#check-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-amount")
+const message = document.querySelector("#error-message")
 
-changeAmount.addEventListener("click",()=>{
-    console.log(billAmount.value);
+
+checkButton.addEventListener("click", () => {
+    message.style.display = "none";
+    if (billAmount.value > 0) {
+       
+        if (cashGiven.value >= billAmount.value) {
+            const payBackAmount = cashGiven.value - billAmount.value;
+            calculateChange(payBackAmount);
+            if (cashGiven.value == billAmount.value) {
+                messageShown("Welcome again");
+            }
+        } else {
+            messageShown("cash should be atleast equal to the bill amount");
+        }
+    } else {
+        messageShown("Nothing is free here!");
+    }
 });
+
+function messageShown(result) {
+    message.style.display = "block";
+    message.innerText = result;
+}
+
+ function calculateChange(payBackAmount){
+
+}
